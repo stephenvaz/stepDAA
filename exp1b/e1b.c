5acc58bb645daa280d27b616d25be61a00d02e91
stephenvaz <stephenvaz2003@gmail.com>
2023-02-11 19:36:31 +0530

    exp 1b complete

--- exp1b/e1b.c
+++ exp1b/e1b.c
@@ -56,59 +56,86 @@ double selection(int a[], int n) {
         // getchar();
     }
     fclose(fp);
+    fp = fopen("./selection.txt", "w+");
+    for(int i = 0; i < n; i++) {
+        fprintf(fp, "%d\n", a[i]);
+    }
+    fclose(fp);
     return totalTime;
 }
+
+void swap(int *x, int *y) {
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+double insertion(int a[], int n) {
+    FILE *fp = fopen("./insertion.csv", "w+");
+    // printf("File opened\n");
+    double totalTime = 0;
+    if(!fp) {
+        printf("Error opening file\n");
+        return -1;
+    }
+    fprintf(fp, "n, time\n");
+    //insertion sort
+    //first sort from 0 to 100 the 0 to 200 and so on upto n
+    for (int i = 100; i <= n; i+=100)
+    {
+        // printf("%d\n", i);
+        clock_t start, end;
+        double cpu_time_used;
+        start = clock();
+        for(int j = 1; j < i; j++)
+        {
+            int k = j;
+            // printf("%d\n", i);
+            while(k > 0 && a[k] < a[k-1])
+            {
+                swap(&a[k], &a[k-1]);
+                k--;
+            }
+        }
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        totalTime += cpu_time_used;
+        fprintf(fp, "%d, %f\n", i, cpu_time_used);
+        
+        printf("Sorted from 0 to %d in %.2fs\n", i, cpu_time_used);
+
+        // for(int z = 0; z < i; z++) {
+        //     printf("%d\n", a[z]);
+        // }
+        // getchar();
+    }
+    fclose(fp);
+    fp = fopen("./insertion.txt", "w+");
+    for(int i = 0; i < n; i++) {
+        fprintf(fp, "%d\n", a[i]);
+    }
+    fclose(fp);
+    return totalTime;
+}
+    
 
 
 int main()
 {
-    clock_t start, end;
-    double cpu_time_used;
-    
-    // code goes here
-    //    int n = 100000; 
-    int n = 50000; 
-         // 400000
+    int n = 100000; 
     int a[n],b[n];
-    //populate array with random integers
-    // for(int i = 0; i < n; i++)
-    // {
-    //     int r = rand();
-    //     a[i] = b[i] = r;
-    // }
     double timeToPopulate = populate(a, b, n);
     printf("Time taken to populate: %f\nSorting...\n", timeToPopulate);
-    
-    // //sort array using insertion sort
-    // for(int i = 1; i < n; i++)
-    // {
-    //     int j = i;
-    //     printf("%d\n", i);
-    //     while(j > 0 && a[j] < a[j-1])
-    //     {
-    //         int temp = a[j];
-    //         a[j] = a[j-1];
-    //         a[j-1] = temp;
-    //         j--;
-    //     }
-    // }
 
     //selection sort
     //first sort from 0 to 100 the 0 to 200 and so on upto n
-    double timeToSort = selection(b, n);
-    printf("Array sorted in %.2f\n", timeToSort);
-    // code ends here
-    // end = clock();
-    // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    // printf("Time taken: %f\n", cpu_time_used);
-    
-    
-    // print sorted array
+    double timeToSortI = insertion(a, n);
+    printf("Array sorted by insertion sort in %.2f\n", timeToSortI);
 
-    // for(int i = 0; i < 101; i++)
-    // {
-    //     // if(a[i] < 1000)
-    //     printf("%d\n", a[i]);
-    // }
+    double timeToSortS = selection(b, n);
+    printf("Array sorted by selection sort in %.2f\n", timeToSortS);
+
+    printf("Total time taken to sort: %f\n", timeToSortI + timeToSortS);
+    
     return 0;
 }
